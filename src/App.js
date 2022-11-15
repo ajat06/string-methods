f33440c626bd9178d3c1ee87dc67d1d6c0ea3358
Ajat chib <chibajat@gmail.com>
2022-11-15 14:47:05 +0530

    Submission of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const x = "Ajat"
+  const y = "Dev"
+  const str1 = 'Cats are the best!';
+  const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+  const str = 'Geeks for Geeks'
+  const array = str.split("for");
+  const num=12;
+
+
+  useEffect(() => {
+    console.log(x.concat(y))
+    console.log(str1.endsWith('best!'));
+    console.log(fruits.slice(1, 3));
+    console.log(array);
+    console.log("Output : " + num.toString(2));  
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
